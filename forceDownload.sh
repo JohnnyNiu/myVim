2ec75a165e81d050d2577fdf98c289252a5c4740
JohnnyNiu <johnny.x.niu@gmail.com>
2017-08-30 22:57:29 +1000

    add force download without removing vundle

---
+++ forceDownload.sh
@@ -0,0 +1,7 @@
+#rm -rf ~/.vim/bundle
+#git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp .vimrc ~/.vimrc
+
+# This is alternative to "Launch vim and run :PluginInstall"
+vim +PluginInstall +qall
