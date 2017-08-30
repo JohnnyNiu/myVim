5444d5da3298f05c8d35e3c15dbc0f59fe31a0a1
JohnnyNiu <johnny.x.niu@gmail.com>
2017-08-30 22:59:06 +1000

    backup vimrc before force download

--- forceDownload.sh
+++ forceDownload.sh
@@ -1,7 +1,8 @@
 #rm -rf ~/.vim/bundle
 #git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cp .vimrc ~/.vimrc
+cp -f ~/.vimrc ~/.vimrc_bkup
+cp -f .vimrc ~/.vimrc
 
 # This is alternative to "Launch vim and run :PluginInstall"
 vim +PluginInstall +qall
